ffacc370132604dbd57efa81816474f4f5d17e93
Telluric-86 <36004802+Telluric-86@users.noreply.github.com>
2018-10-06 22:01:27 +0300

    Сохранение нетбина

--- js/jq.js
+++ js/jq.js
@@ -96,6 +96,34 @@
 
   };
 
+  function addJNode() {
+//    let node = document.createElement('div');
+//    node.className = 'new-div';
+//    node.appendChild( document.createTextNode('Я Ретро-Нода!') );
+    let node = $('<div>', {'class':'new-div'});
+    node.text('Я нодо!');
+    $('main').append(node);
+  };
+
+  function eventsWithNode() {
+    let node = $('<div>',{'class':'div-wraper'});
+    $('div').wrap(node);
+//    $('.new-div').empty();
+  };
+
+  function getCoord() {
+    let str = '';
+
+    str = 'window size =>' + $(window).width() + '*' + $(window).height() + '\n';
+    str += 'document size =>' + $(document).width() + '*' + $(document).height() + '\n';
+    str += '#block-live =>' + JSON.stringify( $('#block-live').offset() ) + '\n';
+    $('#block-live').offset({'top': 300, 'left': 200});
+    str += '#block-live 2 =>' + JSON.stringify( $('#block-live').offset() ) + '\n';
+    
+    alert(str);
+    console.log(str);
+  };
+
 $('#b1').on('click', switchDiv);
 $('#b2').on('click', switchAttr);
 $('#b3').on('click', switchBG);
@@ -104,7 +132,10 @@ $('#b4').on('click1.namespace1', null, 'namespace1', test);
 $('#b4').on('click1.namespace2', null, 'namespace2', test);
 $('.block-cont').on('click', testPropagation);
 $('#b5').on('click', initLive);
-$('#block-live').on('click','a', testLive);
+$('#block-live').on('click','a[href^=\\.]', testLive);
 $('#b6').on('click', animation);
+$('#b7').on('click', addJNode);
+$('#b8').on('click', eventsWithNode);
+$('#b9').on('click', getCoord);
 
 })(jQuery);
